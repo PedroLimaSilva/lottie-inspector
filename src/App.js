7f211d420cc6658859b32ac372630405d45b2f4d
Pedro Lima e Silva <pedro.silva2@sky.uk>
2020-02-04 19:14:52 +0000

    Added Playground

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Player } from './components/Player';
+import { Playground } from './components/Playground';
 
 function App() {
   return (
     <div className="App">
       <Player />
+      <Playground />
     </div>
   );
 }
---
+++ src/components/Playground/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export class Playground extends React.Component {
+  render() {
+    return (
+      <div className="playground">
+        {/**
+         * your attempt goes here
+         */}
+      </div>
+    );
+  }
+}
